59752e6cf7a18903b95e5e7d79dad700f6b3c8b7
zengyuqing0903 <3372284665@qq.com>
2019-11-11 08:23:35 +0800

    message

--- Blog/routes/index.js
+++ Blog/routes/index.js
@@ -4,19 +4,26 @@ var users = require('../data.json').users;
 var chapterList = require('../data.json').chapterList;
 
 /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 
-router.get('/',function(req,res,next){
-  res.render("login");
-})    
-router.get('/login',function(req,res,next){
-  res.writeHead(200,{"Content-Type":"text/plain"});
-  res.write(JSON.stringify(users));
-  res.end();  
-})
-router.get('/list',function(req,res,next){
-  res.render("list",{chapterList:chapterList});
-}) 
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+router.post('/login', function(req, res, next) {
+  var username=req.body.username;
+  var pwd=req.body.pwd; 
+  var userList = JSON.stringify(users);
+  var userlists = JSON.parse(userList);
+  if(userlists[0].username == username && userlists[0].password == pwd){
+    res.end(JSON.stringify({"status":1}));
+  }else{
+    res.end(JSON.stringify({"status":0}));
+    res.end("login error");
+  };
+});
+router.get('/listmanager', function(req, res, next) {
+  res.render('list',{chapterList:chapterList});
+});
 module.exports = router;
